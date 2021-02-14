94ccb31855c81aadabd8fbdde1cc291a0f1ac82b
Iqbal <iqbalnugraha347@gmail.com>
2021-02-14 20:36:27 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomanShayna from "@/components/WomanShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnerShayna,
+    Footer
+  },
+};
+</script>
